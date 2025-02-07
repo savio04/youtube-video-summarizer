4670b9d363c9727d14c7db1454d26d9a7463a7c8
savio04 <savio27.05.2001@gmail.com>
2025-02-07 18:29:41 -0300

    Feat: Improving the UI

--- ui/script.js
+++ ui/script.js
@@ -1,12 +1,67 @@
-function getResume() {
-  const inputElement = document.getElementById("url")
+const bodyElement = document.body
 
+if (localStorage.getItem("theme") === "dark") {
+  bodyElement.classList.remove("light-theme");
+  bodyElement.classList.add("dark-theme");
+}
+
+const inputElement = document.getElementById("url")
+const errorElement = document.getElementById("error")
+const resultElement = document.getElementById("result")
+const videoElement = document.getElementById("videoyt")
+const loadingElement = document.getElementById("loading")
+
+function verifyLocalStorage() {
+  resultElement.removeAttribute("extenalId")
+  const data = localStorage.getItem("extenalId")
+
+  if (data) {
+    inputElement.value = `https://www.youtube.com/watch?v=${data}`
+    inputElement.setAttribute("disabled", true)
+
+    loading(true)
+    poolingGetVideo(data)
+
+    console.log({ data })
+  }
+}
+
+verifyLocalStorage()
+
+function getExtenalIdFromUrl(url) {
+  const [, possibleExternalId] = url.split("v=")
+
+  const [extenalId,] = possibleExternalId.split("&");
+
+  return extenalId
+}
+
+function loading(value) {
+  if (value) {
+    loadingElement.style.display = "flex"
+  } else {
+    loadingElement.style.display = "none"
+  }
+}
+
+function getResume() {
   if (inputElement.value.trim() === "") {
-    const errorElement = document.getElementById("error")
     errorElement.innerText = "Url invalida"
 
     return
   }
+
+  const extenalId = getExtenalIdFromUrl(inputElement.value)
+
+  const alredyResult = resultElement.getAttribute("externalId")
+
+  if (alredyResult && alredyResult === extenalId) return
+
+  localStorage.setItem("extenalId", extenalId)
+
+  inputElement.setAttribute("disabled", true)
+
+  loading(true)
 
   fetch("http://localhost:8080/v1/videos", {
     method: "POST",
@@ -21,16 +76,7 @@ function getResume() {
       }
     })
     .then(data => {
-      if (data.payload.status !== "COMPLETED") {
-        return poolingGetVideo(data.payload.external_id)
-      }
-
-      const resultElement = document.getElementById("result")
-      const videoElement = document.getElementById("videoyt")
-
-      const text = formatText(data.payload.summary)
-      resultElement.innerHTML = `<div>${text}</div>`
-      videoElement.src = `https://www.youtube.com/embed/${data.payload.external_id}`
+      poolingGetVideo(data.payload.external_id)
     })
     .catch(err => console.log({ err }))
 }
@@ -46,19 +92,21 @@ function poolingGetVideo(extenalId) {
       if (data.payload.status !== "COMPLETED") {
         return setTimeout(() => poolingGetVideo(extenalId), 1000)
       }
+      localStorage.removeItem("extenalId")
+      inputElement.removeAttribute("disabled")
 
-      const resultElement = document.getElementById("result")
-      const videoElement = document.getElementById("videoyt")
-
-      resultElement.innerHTML = `<p>${data.payload.summary}</p>`
+      const text = formatText(data.payload.summary)
+      resultElement.innerHTML = text
       videoElement.src = `https://www.youtube.com/embed/${data.payload.external_id}`
+      videoElement.style.display = "block"
+      loading(false)
     })
 }
 
 function formatText(text) {
   const items = text.split(".")
 
-  const groupSize = 6
+  const groupSize = 5
   let groupCount = 0
 
   let newText = ""
@@ -84,4 +132,18 @@ function formatText(text) {
   }
 
   return newText
+}
+
+function toggleTheme() {
+  if (bodyElement.classList.contains("dark-theme")) {
+    bodyElement.classList.remove("dark-theme");
+    bodyElement.classList.add("light-theme");
+
+    localStorage.setItem("theme", "light")
+  } else {
+    bodyElement.classList.remove("light-theme");
+    bodyElement.classList.add("dark-theme");
+
+    localStorage.setItem("theme", "dark")
+  }
 }
