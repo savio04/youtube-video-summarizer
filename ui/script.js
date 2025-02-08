152b08bfbf8fe98e93b9a199fc31a6dc5846fb14
savio04 <savio27.05.2001@gmail.com>
2025-02-08 17:48:14 -0300

    Feat: First ui version

--- ui/script.js
+++ ui/script.js
@@ -1,8 +1,11 @@
 const bodyElement = document.body
+const toggleSlider = document.querySelector('.toggle-slider i')
 
 if (localStorage.getItem("theme") === "dark") {
   bodyElement.classList.remove("light-theme");
   bodyElement.classList.add("dark-theme");
+
+  toggleSlider.className = "fas fa-moon"
 }
 
 const inputElement = document.getElementById("url")
@@ -10,10 +13,11 @@ const errorElement = document.getElementById("error")
 const resultElement = document.getElementById("result")
 const videoElement = document.getElementById("videoyt")
 const loadingElement = document.getElementById("loading")
+const footerElement = document.getElementById("footer")
 
 function verifyLocalStorage() {
-  resultElement.removeAttribute("extenalId")
   const data = localStorage.getItem("extenalId")
+  resultElement.removeAttribute("extenalId")
 
   if (data) {
     inputElement.value = `https://www.youtube.com/watch?v=${data}`
@@ -21,12 +25,15 @@ function verifyLocalStorage() {
 
     loading(true)
     poolingGetVideo(data)
-
-    console.log({ data })
   }
 }
 
 verifyLocalStorage()
+
+function isYouTubeUrl(url) {
+  const youtubeRegex = /^(https?:\/\/)?(www\.)?(youtube|youtu|youtube-nocookie)\.(com|be)\/(watch\?v=[a-zA-Z0-9_-]+|(?:v|e(?:mbed)?)\/[a-zA-Z0-9_-]+)(?!.*\/shorts)/i;
+  return youtubeRegex.test(url);
+}
 
 function getExtenalIdFromUrl(url) {
   const [, possibleExternalId] = url.split("v=")
@@ -38,22 +45,33 @@ function getExtenalIdFromUrl(url) {
 
 function loading(value) {
   if (value) {
+    videoElement.style.display = "none"
+    resultElement.style.display = "none"
+    footerElement.style.display = "none"
     loadingElement.style.display = "flex"
   } else {
     loadingElement.style.display = "none"
   }
 }
 
-function getResume() {
+async function getResume() {
   if (inputElement.value.trim() === "") {
-    errorElement.innerText = "Url invalida"
+    inputElement.classList.add("error")
+    errorElement.innerText = "Url obrigatória"
+
+    return
+  }
+
+  if (!isYouTubeUrl(inputElement.value)) {
+    inputElement.classList.add("error")
+    errorElement.innerText = "Url inválida"
 
     return
   }
 
   const extenalId = getExtenalIdFromUrl(inputElement.value)
 
-  const alredyResult = resultElement.getAttribute("externalId")
+  const alredyResult = resultElement.getAttribute("extenal_id")
 
   if (alredyResult && alredyResult === extenalId) return
 
@@ -63,44 +81,49 @@ function getResume() {
 
   loading(true)
 
-  fetch("http://localhost:8080/v1/videos", {
+  const response = await fetch("http://localhost:8080/v1/videos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({ url: inputElement.value })
   })
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      }
-    })
-    .then(data => {
-      poolingGetVideo(data.payload.external_id)
-    })
-    .catch(err => console.log({ err }))
+
+  if (!response.ok) {
+    return
+  }
+
+  const data = await response.json()
+
+  await poolingGetVideo(data.payload.external_id)
 }
 
-function poolingGetVideo(extenalId) {
-  fetch(`http://localhost:8080/v1/videos/${extenalId}`)
-    .then(response => {
-      if (response.ok) {
-        return response.json()
-      }
-    })
-    .then(data => {
-      if (data.payload.status !== "COMPLETED") {
-        return setTimeout(() => poolingGetVideo(extenalId), 1000)
-      }
-      localStorage.removeItem("extenalId")
-      inputElement.removeAttribute("disabled")
+async function poolingGetVideo(extenalId) {
+  const response = await fetch(`http://localhost:8080/v1/videos/${extenalId}`)
 
-      const text = formatText(data.payload.summary)
-      resultElement.innerHTML = text
-      videoElement.src = `https://www.youtube.com/embed/${data.payload.external_id}`
-      videoElement.style.display = "block"
-      loading(false)
-    })
+  if (!response.ok) {
+    return
+  }
+
+  const data = await response.json()
+
+  if (data.payload.status === "COMPLETED") {
+    localStorage.removeItem("extenalId")
+    inputElement.removeAttribute("disabled")
+
+    const text = formatText(data.payload.summary)
+
+    resultElement.innerHTML = `<h2>Resumo</h2> ${text}`
+    resultElement.setAttribute("extenal_id", data.payload.external_id)
+    resultElement.style.display = "flex"
+    videoElement.src = `https://www.youtube.com/embed/${data.payload.external_id}`
+    videoElement.style.display = "block"
+    footerElement.style.display = "block"
+
+    loading(false)
+  } else {
+    setTimeout(() => poolingGetVideo(extenalId), 1500)
+  }
 }
 
 function formatText(text) {
@@ -128,7 +151,7 @@ function formatText(text) {
   }
 
   if (groupCount > 0) {
-    newText += `<p>${group}</p>`;
+    newText += `<p>${group}</p>`
   }
 
   return newText
@@ -136,14 +159,24 @@ function formatText(text) {
 
 function toggleTheme() {
   if (bodyElement.classList.contains("dark-theme")) {
-    bodyElement.classList.remove("dark-theme");
-    bodyElement.classList.add("light-theme");
+    bodyElement.classList.remove("dark-theme")
+    bodyElement.classList.add("light-theme")
+
+    toggleSlider.className = "fas fa-sun"
 
     localStorage.setItem("theme", "light")
   } else {
-    bodyElement.classList.remove("light-theme");
-    bodyElement.classList.add("dark-theme");
+    bodyElement.classList.remove("light-theme")
+    bodyElement.classList.add("dark-theme")
+
+    toggleSlider.className = "fas fa-moon"
+
 
     localStorage.setItem("theme", "dark")
   }
+}
+
+function clearInputError() {
+  inputElement.classList.remove("error")
+  errorElement.innerText = ""
 }
